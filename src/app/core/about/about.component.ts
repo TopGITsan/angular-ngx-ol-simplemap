01b43de879fa6e7ecc7855dc042592b1ef629353
TopGitSan <c.toparcean@gmail.com>
2021-02-13 20:25:40 +0100

    create home and about component

---
+++ src/app/core/about/about.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit {
+  public title = 'angular-ngx-ol-simplemap';
+  constructor() { }
+
+  ngOnInit(): void {}
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, AboutComponent],
   imports: [
+    CommonModule,
     SharedModule
   ]
 })
