1cee985f9f8bcb0d45b160eef268399e66d17cf0
TopGitSan <c.toparcean@gmail.com>
2021-02-14 19:33:04 +0100

    create a map component, add it to the home comp

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { OlMapComponent } from '../map/ol-map/ol-map.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent, OlMapComponent],
   imports: [
     SharedModule
   ]
--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {View, Feature, Map } from 'ol';
+import { Coordinate } from 'ol/coordinate';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  center: Coordinate= [4558595, 1555957];
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public onMapReady(map: Map) {
+    console.log("Map Ready", map);
+  }
+
+  public setCenter(){
+    console.log("set center");
   }
 
 }
